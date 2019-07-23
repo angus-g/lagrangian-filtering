39de8e3135741d5623bb92ca742627163b4bfb67
Angus Gibson <angus@agibson.me>
2019-07-23 11:28:42 +1000

    First pass at Lagrangian filtering framework
    This is the first steps toward building a framework for flexible
    Lagrangian filtering of ocean model data. The current code is intended
    to transparently set up OceanParcels to run the filtering algorithm:
    creation of a fieldset encapsulating the input data, generation of a
    particle class and kernel to sample fields of interest, and a method
    to run the advection forwards and backwards through the filter window.
    
    Not yet implemented is the highpass filtering of the particle data and
    output to a final file.
    
    This uses a custom branch of OceanParcels that integrates code from
    two pull requests (https://github.com/OceanParcels/parcels/pull/497
    and https://github.com/OceanParcels/parcels/pull/605), which provide
    faster output during advection by using temporary files, and faster
    input during advection by chunking the input fields.

---
+++ filtering/filtering.py
@@ -0,0 +1,152 @@
+import numpy as np
+from datetime import timedelta
+from parcels import FieldSet, ParticleSet, JITParticle, Variable
+from parcels import AdvectionRK4, Kernel
+
+
+def ParticleFactory(variables, name="SamplingParticle", BaseClass=JITParticle):
+    """Create a Particle class that samples the specified variables.
+
+    variables is a dictionary mapping variable names to a field from which
+    the initial value of the variable should be sampled. The variable
+    attributes on the Particle class are prepended by 'var_'."""
+
+    var_dict = {
+        "var_" + v: Variable("var_" + v, initial=f) for v, f in variables.items()
+    }
+
+    newclass = type(name, (BaseClass,), var_dict)
+    return newclass
+
+
+class LagrangeFilter(object):
+    """The main Lagrangian filter class, holds all the required state.
+    """
+
+    def __init__(
+        self,
+        name,
+        filenames,
+        variables,
+        dimensions,
+        sample_variables,
+        mesh="flat",
+        uneven_window=False,
+        window_size=None,
+        highpass_frequency=5e-5,
+        advection_dt=timedelta(minutes=5),
+    ):
+        # The name of this filter
+        self.name = name
+        # Width of window over which our filter computes a meaningful result
+        # in seconds. Default to 3.5 days on either side
+        if window_size is None:
+            self.window_size = timedelta(days=3.5).total_seconds()
+        else:
+            self.window_size = window_size
+        # Whether we're permitted to use uneven windows on either side
+        self.uneven_window = uneven_window
+
+        # construct the OceanParcels FieldSet
+        # to use for particle advection
+        self.fieldset = FieldSet.from_netcdf(
+            filenames, variables, dimensions, mesh=mesh, deferred_load=False
+        )
+
+        # guess the output timestep
+        times = self.fieldset.gridset.grids[0].time
+        self.output_dt = times[1] - times[0]
+
+        # timestep for advection
+        self.advection_dt = advection_dt
+
+        # map sampled variables to fields
+        self.sample_fields = {v: getattr(self.fieldset, v) for v in sample_variables}
+        # create the particle class and kernel for sampling
+        self.particleclass = ParticleFactory(self.sample_fields)
+        self.create_sample_kernel()
+        self.kernel = AdvectionRK4 + self.sample_kernel
+
+    def create_sample_kernel(self):
+        """Create the parcels kernel for sampling fields during advection."""
+
+        # make sure the fieldset has C code names assigned, etc.
+        self.fieldset.check_complete()
+
+        # string for the kernel itself
+        f_str = "def sample_kernel(particle, fieldset, time):\n"
+        for v in self.sample_fields.keys():
+            f_str += f"\tparticle.var_{v} = fieldset.{v}[time, particle.depth, particle.lat, particle.lon]\n"
+
+        # create the kernel
+        self.sample_kernel = Kernel(
+            self.fieldset,
+            self.particleclass.getPType(),
+            funcname="sample_kernel",
+            funcvars=["particle", "fieldset", "time"],
+            funccode=f_str,
+        )
+
+    def particleset(self, time):
+        """Create a particleset initialised at the given time."""
+
+        # we make the assumption that the grid is rectilinear for the moment
+        lon, lat = np.meshgrid(
+            self.fieldset.gridset.grids[0].lon, self.fieldset.gridset.grids[0].lat
+        )
+
+        return ParticleSet(
+            self.fieldset, pclass=JITParticle, lon=lon, lat=lat, time=time
+        )
+
+    def filter_step(self, time_index, time):
+        """Perform forward-backward advection at a single timestep."""
+
+        # seed all particles at gridpoints and advect forwards
+        ps = self.particleset(time)
+        outfile_forward = ps.ParticleFile(
+            "{}_{}_forward".format(self.name, time_index), outputdt=self.output_dt
+        )
+        ps.execute(
+            self.kernel,
+            runtime=self.window_size,
+            dt=self.advection_dt,
+            output_file=outfile_forward,
+        )
+
+        # reseed particles, but advect backwards (using negative dt)
+        ps = self.particleset(time)
+        outfile_backward = ps.ParticleFile(
+            "{}_{}_backward".format(self.name, time_index), outputdt=self.output_dt
+        )
+        ps.execute(
+            self.kernel,
+            runtime=self.window_size,
+            dt=-self.advection_dt,
+            output_file=outfile_backward,
+        )
+
+        # filter data from output files and write back this timestep
+        # first, tell the output files to not automatically export to netcdf
+        outfile_forward.to_export = False
+        outfile_backward.to_export = False
+
+    def __call__(self, times=None):
+        """Run the filtering process on this experiment."""
+
+        # run over the full range of valid time indices unless specified otherwise
+        if times is None:
+            times = self.fieldset.gridset.grids[0].time
+
+            if self.uneven_window:
+                raise NotImplementedError("uneven windows aren't supported")
+
+        # restrict to period covered by window
+        times = np.array(times)
+        window_left = times - times[0] >= self.window_size
+        window_right = times <= times[-1] - self.window_size
+        times = times[window_left & window_right]
+
+        # do the filtering at each timestep
+        for idx, time in enumerate(times):
+            self.filter_step(idx, time)
