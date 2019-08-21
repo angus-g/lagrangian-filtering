317b6086abf3e23c698cffc9c16374afb44c25f0
Angus Gibson <angus@agibson.me>
2019-08-21 12:00:07 +1000

    Add Conda build file

--- filtering/__init__.py
+++ filtering/__init__.py
@@ -1,1 +1,8 @@
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    __version__ = get_distribution("lagrangian-filtering").version
+except DistributionNotFound:
+    pass
+
 from .filtering import LagrangeFilter
--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
 from setuptools import setup, find_packages
 
-setup(name="lagrangian-filtering", version="0.1.dev", packages=find_packages())
+setup(
+    name="lagrangian-filtering",
+    description="Temporal filtering of data in a Lagrangian frame of reference",
+    author="Angus Gibson",
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    packages=find_packages(),
+)
