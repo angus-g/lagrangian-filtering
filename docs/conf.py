dd1051cdf4d56e2d66cd9bd4c9545b5c2879e1ca
Angus Gibson <angus@agibson.me>
2020-07-27 16:15:45 +1000

    Update copyright years

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath(".."))
 # -- Project information -----------------------------------------------------
 
 project = "lagrangian-filtering"
-copyright = "2019, Angus Gibson"
+copyright = "2019-2020, Angus Gibson"
 author = "Angus Gibson"
 
 # The short X.Y version
