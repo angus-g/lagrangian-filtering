a8dacee257ae48169dd12f47ac941ab253227e29
Angus Gibson <angus@agibson.me>
2021-11-16 10:57:37 +1100

    Add the data-dependent filtering class
    We also export all the filters in __all__, so they can be picked up by
    `from filter import *`, and perhaps more importantly, by sphinx.

--- filtering/filter/__init__.py
+++ filtering/filter/__init__.py
@@ -1,2 +1,5 @@
 from .filter import Filter, FrequencySpaceFilter
 from .spatial_filter import SpatialFilter
+from .data_filter import DataDependentFilter
+
+__all__ = ["Filter", "FrequencySpaceFilter", "SpatialFilter", "DataDependentFilter"]
